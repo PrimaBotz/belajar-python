0675e33bff24f3f394c4e0bf88f8b6e3ac4183a0
PrimaAye <168735174+PrimaBotz@users.noreply.github.com>
2025-03-23 13:52:00 +0800

    y

---
+++ main.py
@@ -0,0 +1,10 @@
+print("hai")
+# Ini adalah comment
+print("canda")
+if 4 > 1:
+    print("Empat Lebih Besar Daripada 1")
+a = 12
+print(a)
+
+#Ini adalah cara mengcompile python ke bytecode
+#python -m py_compile Main.py (Ini Adalah Caranya) - Ketik Ini Di TERMINAL 
