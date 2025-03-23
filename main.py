b7730e535fa0a33c01b626a3108b2df4e7c83c27
PrimaAye <168735174+PrimaBotz@users.noreply.github.com>
2025-03-23 22:39:47 +0800

    untung

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-print("hai")
-# Ini adalah comment
-print("canda")
-if 4 > 1:
-    print("Empat Lebih Besar Daripada 1")
-a = 12
-print(a)
+
+a = 19.5
+b = 30
+c = 15
+print(a + b - c)
+# Materi Aritmatika
+
+
+nama = input("Siapa namanya?")
+print("Halo.." + nama)
+# Input User
+
+nama = "Babi"
+umur = 12
+tinggi = 170
+
+print("hai nama saya" + nama)
+print(umur)
+# Tempat Penyimpanan Variabel
+
+umur = int(input("Berapa Umur kamu?"))
+if umur >= 17:
+    print("boleh buat ktp")
+else:
+    print("Belum Boleh Buat Ktp")
+#belajar if else
+
+for papi in range(10):
+    print("oon")
+#Looping / Perulangan 
+
+x = 2
+while x < 10:
+    print("Nilai x:", x)
+    x += 5
+
+    
+
 
 #Ini adalah cara mengcompile python ke bytecode
 #python -m py_compile Main.py (Ini Adalah Caranya) - Ketik Ini Di TERMINAL 
